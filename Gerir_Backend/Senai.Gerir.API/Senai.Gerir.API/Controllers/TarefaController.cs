9ea43a7cacb092aef95c54ff1fe0f259d7256ef1
nicolasgabrieloficial <61585068+nicolasgabrieloficial@users.noreply.github.com>
2021-01-06 17:42:27 -0300

    TESTEZADA

---
+++ Gerir_Backend/Senai.Gerir.API/Senai.Gerir.API/Controllers/TarefaController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Gerir.API.Dominios;
+using Senai.Gerir.API.Interfaces;
+using Senai.Gerir.API.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Gerir.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        private readonly ITarefaRepositorio _tarefarepositorio;
+
+        public TarefaController()
+        {
+            _tarefarepositorio = new TarefaRepositorio();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Tarefa tarefa)
+        {
+            try
+            {
+                _tarefarepositorio.Cadastrar(tarefa);
+
+                return Ok(tarefa);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //[Authorize]
+        [HttpGet("{IdUsuario}")]
+        public IActionResult MinhasTarefas(Guid IdUsuario)
+        {
+            try
+            {
+                var Listadetarefas = _tarefarepositorio.ListarTodos(IdUsuario);
+
+                return Ok(Listadetarefas);
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpPut]
+        public IActionResult Editar(Tarefa tarefa)
+        {
+            try
+            {   
+                _tarefarepositorio.Editar(tarefa);
+                return Ok(tarefa);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+        [Authorize]
+        [HttpDelete("{Idtarefa}")]
+
+        public IActionResult Remover(Guid Idtarefa)
+        {
+            try
+            {
+
+                _tarefarepositorio.Remover(Idtarefa);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+         
+        [HttpPut("alterarstatus/{IdTarefa}")]
+        public IActionResult AlterarStatus(Guid Idtarefa)
+        {
+            try
+            {
+
+                _tarefarepositorio.AlterarStatus(Idtarefa);
+
+                return Ok(Idtarefa);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Gerir_Backend/Senai.Gerir.API/Senai.Gerir.API/Interfaces/ITarefasRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Senai.Gerir.API.Dominios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Gerir.API.Interfaces
+{
+    interface ITarefaRepositorio
+    {
+        Tarefa Cadastrar(Tarefa tarefa);
+        List<Tarefa> ListarTodos(Guid IdUsuarios);
+        Tarefa AlterarStatus(Guid IdTarefa);
+        void Remover(Guid Idtarefa);
+        Tarefa Editar(Tarefa tarefa);
+        Tarefa BuscarporId(Guid Idtarefa);
+    }
+}
